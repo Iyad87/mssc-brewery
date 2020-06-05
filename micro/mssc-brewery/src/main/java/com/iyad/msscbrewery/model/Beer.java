234e9821ae9edced153d5fbd9e819b584adbc058
IyadMob <ialtoubah@mobiquityinc.com>
2020-06-05 15:51:45 +0200

    set up model

---
+++ micro/mssc-brewery/src/main/java/com/iyad/msscbrewery/model/Beer.java
@@ -0,0 +1,19 @@
+package com.iyad.msscbrewery.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+    private UUID id;
+    private String name;
+    private String beetStyle;
+    private Long ups;
+}
